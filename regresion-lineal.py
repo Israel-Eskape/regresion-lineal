9c3f8d53ac16ed80c00de542268a1daeaa58990a
Israel-Eskape <isra.ixoye@gmail.com>
2021-10-24 06:47:27 -0500

    Se obtiene la información de cuantas peliculas se lanzaron en cada año

--- regresion-lineal.py
+++ regresion-lineal.py
@@ -1,8 +1,22 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
 from collections import Counter
+
+from pandas.core.frame import DataFrame
 df = pd.read_csv('netflix_titles.csv')
 
-dateTypeName = Counter(df['type']) 
+#dataTypeName = Counter(df['type']) #.keys() and .value()
+#dataTypeRelease = Counter(df['release_year'])
 
 
-print(dateTypeName)
+#movie = df.loc[:,'type'] == 'Movie'
+#moviedf = df.loc[movie]
+
+movie = df.loc[df.loc[:,'type'] == 'Movie']
+movieYear = movie.loc[:,['type','release_year']]
+
+dataCount = Counter(movieYear['release_year'])
+
+
+print(dataCount)
